1393306f5de9afcaec11988826ad14ecaf973833
thiagoafcf <thiago.afcf@gmail.com>
2024-07-16 18:51:57 -0300

    Upload do projeto
    Primeiro upload do projeto com as funções principais básicas funcionais.

---
+++ cartorio_ebac.c
@@ -0,0 +1,310 @@
+#include <stdio.h> //Biblioteca de comunicação com o usuário
+#include <stdlib.h> //Biblioteca de alocação de memória (a entender ainda)
+#include <locale.h> //Biblioteca de alocação de texto por região. Ou seja, usada para poder definir os caracteres de um idioma a ser mostrado ao e usado pelo usuário.
+#include <string.h> //Biblioteca para trabalhar com strings
+#include <ctype.h> //Biblioteca para trabalhar caracteres
+
+#define OPCAO_S 'S'
+#define OPCAO_N 'N'
+#define TAMANHO_CPF 11
+
+int cadastrar()
+{
+	char nome_arquivo[TAMANHO_CPF+1], cpf[TAMANHO_CPF+1], nome[60], sobrenome[60], cargo [20];
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	do
+	{
+		system("cls");
+		printf("--- REGISTRO DE NOVO CADASTRO ---\n\n\n");
+		printf("CPF (Sem pontos e traços): ");
+		scanf("%s", cpf);
+								
+		if(verifica_cpf(cpf) == 0) // Verifica se o CPF contém exatamente 11 dígitos e se é composto apenas por números
+		{
+			printf("CPF inválido.\n\n");
+			system("pause");	
+		}
+	} while (verifica_cpf(cpf) == 0);
+	
+	if(cpf[TAMANHO_CPF + 1] == '\n'){ // if else para remover o caracter de quebra de linha da string e substituir pelo caractere nulo
+			cpf[TAMANHO_CPF + 1] = '\0';
+	}
+	else
+	{
+		int i;
+		while((i = getchar()) != '\n' && i != EOF);
+	}
+		
+	strcpy(nome_arquivo, cpf);
+	
+	FILE *file;
+	file = fopen(nome_arquivo, "w");
+	fprintf(file, cpf);
+	fclose(file);
+	
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+	
+	printf("Nome: ");
+	fgets(nome, sizeof(nome), stdin); //fgets para poder receber espaços
+	nome[strcspn(nome, "\n")] = '\0'; //Função para remover o caractere de quebra de linha e substituir pelo caractere nulo
+			
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, nome);
+	fclose(file);
+	
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+
+	printf("Sobrenome: ");
+	fgets(sobrenome, sizeof(sobrenome), stdin);
+	sobrenome[strcspn(sobrenome, "\n")] = '\0';
+	
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, sobrenome);
+	fclose(file);
+	
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, ",");
+	fclose(file);
+	
+	printf("Cargo: ");
+	fgets(cargo, sizeof(cargo), stdin);
+	cargo[strcspn(cargo, "\n")] = '\0';
+	
+	file = fopen(nome_arquivo, "a");
+	fprintf(file, cargo);
+	fclose(file);
+	
+	printf("\n\nUsuário cadastrado com sucesso.\n");		
+	system("pause");
+}
+
+int consultar()
+{
+	setlocale(LC_ALL,"Portuguese");
+	
+	char cpf[TAMANHO_CPF+1], conteudo[100], virgula[2] = ",", *token;
+	
+	printf("--- CONSULTA DE CADASTRO ---\n\n\n");
+	printf("Informe o CPF para buscar o cadastro: ");
+	scanf("%s", cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if (file == NULL)
+	{
+		printf("Cadastro não localizado.\n\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+	{
+		printf("\nCadastro localizado:\n\n");
+		token = strtok(conteudo, virgula);
+		printf("\tCPF:       %s\n", token);
+		token = strtok(NULL, virgula);
+		printf("\tNome:      %s\n", token);
+		token = strtok(NULL, virgula);
+		printf("\tSobrenome: %s\n", token);
+		token = strtok(NULL, virgula);
+		printf("\tCargo:     %s\n", token);
+		printf("\n");
+	}
+	
+	system("pause");
+}
+
+int deletar()
+{
+	char cpf[TAMANHO_CPF+1], conteudo[100], virgula[2] = ",", *token, opcao;
+	int erro = 1;
+	
+	setlocale(LC_ALL,"Portuguese");
+	printf("--- EXCLUIR CADASTRO ---\n\n\n");
+	printf("Informe o CPF para buscar o cadastro: ");
+	scanf("%s", cpf);
+	
+	if(cpf[TAMANHO_CPF + 1] == '\n'){ // if else para remover o caracter de quebra de linha da string e substituir pelo caractere nulo
+			cpf[TAMANHO_CPF + 1] = '\0';
+	}
+	else
+	{
+		int i;
+		while((i = getchar()) != '\n' && i != EOF);
+	}
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if (file == NULL)
+	{
+		printf("Cadastro não localizado.\n\n");
+		system("pause");
+	}
+	else
+	{
+		while(fgets(conteudo, 200, file) != NULL)
+		{
+			printf("\nCadastro localizado:\n\n");
+			token = strtok(conteudo, virgula);
+			printf("\tCPF:       %s\n", token);
+			token = strtok(NULL, virgula);
+			printf("\tNome:      %s\n", token);
+			token = strtok(NULL, virgula);
+			printf("\tSobrenome: %s\n", token);
+			token = strtok(NULL, virgula);
+			printf("\tCargo:     %s\n", token);
+			printf("\n\n");
+		}
+		
+		do
+		{
+			printf("Deseja excluir este cadastro? S/N: ");
+			scanf("%c", &opcao);
+			getchar();
+			
+					
+			opcao = toupper(opcao); //Função para deixar o caractere em caixa alta
+					
+			switch(opcao)
+			{
+				case OPCAO_S:
+					fclose(file); //Precisa fechar o arquivo para poder deletá-lo
+					remove(cpf); //Remove o arquivo salvo
+					printf("\nCadastro excluído com sucesso.\n");
+					system("pause");
+					erro = 0;
+				break;
+				
+				case OPCAO_N:
+					printf("\nRetornando ao menu principal.\n");
+					system("pause");
+					erro = 0;
+				break;
+				
+				default:
+					printf("\nOpção inválida. Digite 'S' para excluir e 'N' para retornar ao menu principal.\n");
+			}
+		} while(erro == 1);
+	}
+}
+
+int encerrar()
+{
+	char encerrar_programa;
+	
+	do
+	{
+		printf("Deseja encerrar o programa? S/N: ");
+		scanf("%c", &encerrar_programa);
+		getchar();
+		
+		encerrar_programa = toupper(encerrar_programa); //Função para deixar o caractere em caixa alta
+		
+		switch(encerrar_programa)
+		{
+			case OPCAO_S:
+				printf("\nO programa será encerrado.\n");
+				system("pause");
+				return 1;
+			break;
+			
+			case OPCAO_N:
+				printf("\nRetornando ao menu principal.\n");
+				system("pause");
+				return 0;
+			break;
+			
+			default:
+				printf("\nOpção inválida. Digite 'S' para fechar e 'N' para retornar ao menu principal.\n");
+		}									
+	}
+	while(1);
+}
+
+int verifica_cpf(const char *cpf) 
+{
+	int i;
+	
+	if(strlen(cpf) != TAMANHO_CPF) //Verifica se a string tem 11 dígitos
+	{
+        return 0;
+    }
+    for(i = 0 ; i < TAMANHO_CPF; i++) //Verifica se a string contém somente números
+	{
+		if(!isdigit(cpf[i])) 
+		{
+	        return 0;
+	    }
+	}
+    return 1;
+}
+
+/*int verifica_string(const char *string) 
+{
+    int i;
+    
+	for (i = 0; i < strlen(string); i++) 
+	{
+        if (!isalpha(string[i]) && string[i] != ' ') 
+		{
+            return 0; // Retorna 0 se encontrar um caractere que não seja letra nem espaço
+        }
+    }
+    return 1; // Retorna 1 se todos os caracteres forem válidos
+}*/
+
+int main(void)
+{
+	int opcao = 0, fecha = 0;
+			
+	while(fecha == 0)
+	{
+		setlocale(LC_ALL,"Portuguese");
+		
+		system("cls");
+		
+		printf("### CARTÓRIO EBAC ###\n\n");
+		printf("Olá! Digite o número de uma opção para prosseguir:\n\n");
+		printf("\t1 - Registrar novo cadastro\n");
+		printf("\t2 - Consultar cadastro\n");
+		printf("\t3 - Excluir cadastro\n");
+		printf("\t4 - Encerrar programa\n\n");
+		
+		printf("Opção: ");
+		scanf("%d", &opcao);
+		getchar();
+		
+		system("cls");
+		
+		switch(opcao)
+		{
+			case 1:
+				cadastrar();
+			break;
+			
+			case 2:
+				consultar();
+			break;
+			
+			case 3:
+				deletar();
+			break;
+			
+			case 4:
+				fecha = encerrar();	
+			break;
+			
+			default:
+				printf("Opção inválida\n");
+				system("pause");
+			break;
+		}
+	}
+	return 0;
+}
